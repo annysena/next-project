084ca05543c93f3ce8829124a286e9d06fd59951
Anny dos Santos Sena Vaz <annysena.as44@gmail.com>
2023-07-20 15:05:06 -0300

    add css

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,16 @@ export default function Home() {
         <meta name="keyworks" content="Roupas, Calçados, Boné"></meta>
         <meta name="description" content="Encontre a melhor roupa para você"></meta>
       </Head>
-      <div>
-        <h1 className={styles.title}>Ínicio da Página</h1>
-        <Image 
-         src="/image/img2.jpg" 
+      <div className={styles.title}>
+        <h1>Ínicio da Página</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci impedit quibusdam, commodi quos illum tenetur fugiat autem consequuntur dolores provident similique, ipsum, architecto reiciendis nam perferendis eaque praesentium rerum sed?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa quaerat ut fuga autem quos laborum deserunt vel sed aliquam doloribus? Placeat odio odit nesciunt laborum ducimus minus rerum sunt eveniet!</p>
+        {/* <Image 
+         src="/image/green.png" 
          width={400} 
          height={400} 
-         alt="Cidade Noturna"
-        />
+         alt="flowers"
+        /> */}
       </div>
       </>
   )
